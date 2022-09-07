5b5158ef4ed0aaf5f686e77f5d43ae87bf193a07
LUZ <balon.basket.90@gmail.com>
2022-09-06 19:29:08 -0500

    [+] header and hero
    header and hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,23 @@ const Hero = () => {
   return (
     <div className="hero">
         <div className='left-h'>
-         <Header/>
+          <Header/>
+          <div className='the-best-ad'>
+            <div></div>
+            <span> the best itness club in the town</span>
+          </div>
+          <div className='hero-text'>
+            <div>
+              <span className='stroke-text'>Shape</span>
+              <span>Your</span>
+            </div>
+            <div>
+              <span>Ideal body</span>
+            </div>
+            <div>
+              <span>In here we will help you to shape and build our ideal body and life up your life to fullest</span>
+            </div>
+          </div>
         </div>
         <div className='right-h'> right side</div>
     </div>
