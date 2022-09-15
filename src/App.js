eefb62dd1d3d206ba55f5f250c5260de991d0747
LUZ <balon.basket.90@gmail.com>
2022-09-15 14:04:41 -0500

    [+]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './components/Hero/Hero'
 import Hero from './components/Hero/Hero';
+import Programs  from './components/Programs/Programs';
 function App() {
   return (
     <div className="App">
           <Hero/>
+          <Programs/>
     </div>
   );
 }
---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Programs.css'
+export default function Programs () {
+  return (
+    <div>Programs</div>
+  )
+}
